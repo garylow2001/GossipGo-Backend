f33020b6bac7fc51ae2b028ecf1cb601399f03bc
Gary <garylow2001@gmail.com>
2024-01-22 14:18:24 +0800

    Update CORSAllowedOrigin

--- configs/configs.go
+++ configs/configs.go
@@ -5,6 +5,6 @@ import "time"
 var JWTExpirationTime = time.Hour * 72 // 3 days
 var JWTExpirationTimeInSeconds = int(JWTExpirationTime.Seconds())
 
-var CORSAllowedOrigins = []string{"http://localhost:3001"}
+var CORSAllowedOrigins = []string{"https://gossipgo.netlify.app"}
 var CORSAllowedMethods = []string{"GET", "POST", "PUT", "DELETE"}
 var CORSAllowedHeaders = []string{"Origin", "Content-Type", "X-Auth-Token", "Authorization"}
